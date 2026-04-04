e628145f63567802413492f8d0a6ff7b935dc1e0
shuaishuai <1510589255@qq.com>
2026-04-04 11:03:35 +0800

    Add territory sandbox game types

---
+++ territory-sandbox/src/game/types.ts
@@ -0,0 +1,39 @@
+export type Faction = {
+  id: number
+  name: string
+  color: string
+}
+
+export type HexCell = {
+  id: string
+  q: number
+  r: number
+  x: number
+  y: number
+  isLand: boolean
+  ownerId: number | null
+  population: number
+  growthRate: number
+  maxPopulation: number
+  neighbors: string[]
+  wanderSeeds: number[]
+}
+
+export type World = {
+  width: number
+  height: number
+  hexSize: number
+  cells: HexCell[]
+  cellMap: Record<string, HexCell>
+  factions: Faction[]
+  selectedCellId: string | null
+  tick: number
+}
+
+export type SimulationOptions = {
+  growthMultiplier: number
+  conquestIntervalMs: number
+  attackThreshold: number
+  sendRatio: number
+  defenseBonus: number
+}
